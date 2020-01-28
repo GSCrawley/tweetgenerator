7ded74983df34239390177f5855ab0ea55395bb3
GSCrawley <gscrawley@gmail.com>
2020-01-27 16:59:34 -0800

    frequency and unique_words functions functional

---
+++ frequency.py
@@ -0,0 +1,13 @@
+word_histogram = {}
+with open('raven.txt','r') as f:
+    for line in f:
+        for word in line.split():
+            single_word = word
+            word_histogram[word] = word_histogram.get(word, 0) + 1
+
+def frequency(word, histogram):
+    return histogram[word]
+
+print(frequency("Raven", word_histogram))
+
+
---
+++ unique_words.py
@@ -0,0 +1,14 @@
+
+def unique_words():
+    word_histogram = {}
+    with open('raven.txt','r') as f:
+        for line in f:
+            for word in line.split():
+                single_word = word
+                word_histogram[word] = word_histogram.get(word, 0) + 1
+                unique_words = 0
+        for word in word_histogram.keys():
+            if word_histogram[word] == 1:
+                unique_words += 1   
+        return(unique_words)                
+print(unique_words())
